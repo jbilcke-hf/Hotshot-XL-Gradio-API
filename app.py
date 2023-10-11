b7fda75e58219657dfd6deee79798a58ffa13756
Sylvain Filoni <sylvain.fillion15@gmail.com>
2023-10-11 16:23:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,24 @@ def infer (prompt):
 
   return "output.gif"
 
-demo = gr.Interface(fn=infer, inputs="textbox", outputs="file")
-demo.launch()
+css="""
+#col-container{
+    margin: 0 auto;
+    max-width: 720px;
+    text-align: left;
+}
+"""
+
+with gr.Blocks(css=css) as demo:
+  with gr.Column(elem_id="col-container"):
+    gr.HTML("""
+<h2 style="text-align: center;">Hotshot-XL Text to GIF</h2>
+<p style="text-align: center;">Hotshot-XL is an AI text-to-GIF model trained to work alongside Stable Diffusion XL</p>
+        """)
+    prompt = gr.Textbox(label="Prompt")
+    submit_btn = gr.Button("Submit")
+    gif_result = gr.Image(label="Gif result")
+  submit_btn.click(fn=infer, inputs=[prompt], outputs=[gif_result])
+
+demo.queue(max_size=12).launch()
     
