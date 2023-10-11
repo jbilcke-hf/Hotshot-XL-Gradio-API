9d4bdf5be799caf9176a5588e165b415ee718b26
Sylvain Filoni <sylvain.fillion15@gmail.com>
2023-10-11 15:48:54 +0200

    add gradio app

---
+++ app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+
+def infer (prompt):
+  command = [
+        "python",
+        "inference.py",
+        f"--prompt={prompt}",
+        "--output=output.gif" 
+    ]
+
+  try:
+      subprocess.run(command, check=True)
+  except subprocess.CalledProcessError as e:
+      print(f"An error occurred: {e}")
+
+  return "output.gif"
+
+demo = gr.Interface(fn=infer, inputs="textbox", outputs="file")
+demo.launch()
+    
