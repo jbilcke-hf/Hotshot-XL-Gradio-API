100af6e9eb2187fce933d39d192adbfd9dd5879f
Sylvain Filoni <sylvain.fillion15@gmail.com>
2023-10-12 13:32:54 +0200

    Update app_w_lora.py

--- app_w_lora.py
+++ app_w_lora.py
@@ -2,75 +2,83 @@ import hashlib
 import os
 import uuid
 import requests
-import tempfile
 import pathlib
+import subprocess
+import gradio as gr
+from huggingface_hub import snapshot_download, HfFileSystem
 
-# Hash the url to get a unique filename for caching
-def hash_url(url: str) -> str:
-    hash_object = hashlib.sha1(url.encode())
-    return hash_object.hexdigest()
-
-# download a file URL (1st param) to a specific path (2nd param)
-def download_file(url: str, dest: str) -> None:
-    response = requests.get(url, stream=True)
-    response.raise_for_status()
-    with open(dest, "wb") as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
-
-# delete a file or folder
-def delete_file_if_exists(filepath: str) -> None:
-    os.remove(filepath) if os.path.exists(filepath) else None
+fs = HfFileSystem()
 
 # execute a CLI command
 def execute_command(command: str) -> None:
     subprocess.run(command, check=True)
 
-# create a temporary file name in the /tmp folder
-def create_temp_file(prefix: str, postfix: str) -> str:
-    return f"{tempfile.gettempdir()}/{prefix}{uuid.uuid4().hex}{postfix}"
+# Download stable-diffusion-xl
+local_dir = f"./stablediff"
+snapshot_download(
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        local_dir=local_dir,
+        repo_type="model",
+        ignore_patterns=".gitattributes",
+        #token=hf_token
+    )
+spatial_unet_base = f"./stablediff/unet"
 
 
-import subprocess
-import gradio as gr
+def get_files(file_paths):
+    last_files = {}  # Dictionary to store the last file for each path
 
-storage_path = os.getenv("STORAGE_PATH", './sandbox')
-loras_dir_file_path = os.path.join(storage_path, "loras")
-models_dir_file_path = os.path.join(storage_path, "models")
+    for file_path in file_paths:
+        # Split the file path into directory and file components
+        directory, file_name = file_path.rsplit('/', 1)
+    
+        # Update the last file for the current path
+        last_files[directory] = file_name
+    
+    # Extract the last files from the dictionary
+    result = list(last_files.values())
 
-base_sdxl_dir = os.path.join(models_dir_file_path, "stable-diffusion-xl-base-1.0")
-spatial_unet_base_global_var = os.path.join(base_sdxl_dir, "unet")
+    return result
+
+
+def load_lora(lora_id):
+    # List all ".safetensors" files in repo
+    sfts_available_files = fs.glob(f"{custom_model}/*safetensors")
+    sfts_available_files = get_files(sfts_available_files)
+
+    if sfts_available_files == []:
+        sfts_available_files = ["NO SAFETENSORS FILE"]
+
+    print(f"Safetensors available: {sfts_available_files}")
+    
+    return sfts_available_files[0]
+
 
 def infer(prompt: str, lora: str = None, size: str = '512x512'):
     width, height = map(int, size.split('x'))
 
-    # This will ensure all necessary directories are created
-    pathlib.Path(loras_dir_file_path).mkdir(parents=True, exist_ok=True)
-
-    lora_path = os.path.join(loras_dir_file_path, hash_url(lora) + ".safetensors")
-
     if lora:  # only download if a link is provided
         lora = lora.strip()  # remove leading and trailing white spaces
-        lora_path = os.path.join(loras_dir_file_path, hash_url(lora) + ".safetensors")
-        download_file(lora, lora_path)
+        if lora == "NO SAFETENSORS FILE" : 
+            lora_path = None
+        else:
+            lora_path = load_lora(lora)
     else:
         lora_path = None
 
-    output = create_temp_file(prefix="output_", postfix=".gif")
+    output = "output.gif"
 
     command = [
       f"python", 
       f"inference.py",
       f"--prompt={prompt}", 
       f"--output={output}",
-      f"--spatial_unet_base={spatial_unet_base_global_var}",
       f"--width={width}",
       f"--height={height}"
     ]
 
     if lora:
-        lora_path = os.path.join(loras_dir_file_path, hash_url(lora) + ".safetensors")
-        download_file(lora, lora_path)
+        command.append(f"--spatial_unet_base={spatial_unet_base}")
         command.append(f"--lora={lora_path}")
 
     execute_command(command)
@@ -94,7 +102,7 @@ with gr.Blocks(css=css) as demo:
         <p style="text-align: center;">Hotshot-XL is an AI text-to-GIF model trained to work alongside Stable Diffusion XL</p>
                 """)
         prompt = gr.Textbox(label="Prompt")
-        lora = gr.Textbox(label="LoRA", default=None)
+        lora = gr.Textbox(label="LoRA", value=None)
         size = gr.Radio(label="Size", choices=[
             '320x768',
             '384x672',
@@ -103,7 +111,7 @@ with gr.Blocks(css=css) as demo:
             '608x416',
             '672x384',
             '768x320'
-        ], default='512x512')
+        ], value='512x512')
         submit_btn = gr.Button("Submit")
         gif_result = gr.Image(label="Gif")
     submit_btn.click(fn=infer, inputs=[prompt, lora, size], outputs=[gif_result])
