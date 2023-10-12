49dfc53f43ee80d78830b235b8f0c1a476af3299
Sylvain Filoni <sylvain.fillion15@gmail.com>
2023-10-12 15:19:48 +0200

    Update app_w_lora.py

--- app_w_lora.py
+++ app_w_lora.py
@@ -74,7 +74,8 @@ def infer(prompt: str, negative_prompt: str, lora: str = None, size: str = '512x
     command = [
       f"python", 
       f"inference.py",
-      f"--prompt={prompt}", 
+      f"--prompt={prompt}",
+      f"--negative_prompt={negative_prompt}",
       f"--output={output}",
       f"--width={width}",
       f"--height={height}",
